0f1f1571d3fdacd54a5497035e83a37d11d3f8dd
Philipp <philipp.pichler@hotmail.com>
2018-12-19 22:34:06 +0100

    small refactoring

--- day5/start.py
+++ day5/start.py
@@ -1,8 +1,5 @@
-from datetime import datetime
-
 with open('input', 'r') as input:
     polymer = input.read()
-    start_time = datetime.now()
 
 
     def react(polymer):
@@ -10,11 +7,8 @@ with open('input', 'r') as input:
         while found:
             found = False
             for c in range(97, 123):
-                letter = chr(c)
-                letter_u = letter.upper()
-                c1 = letter + letter_u
+                c1 = chr(c) + chr(c).upper()
                 c2 = c1[::-1]
-
                 if c1 in polymer or c2 in polymer:
                     polymer = polymer.replace(c1, '').replace(c2, '')
                     found = True
@@ -22,17 +16,11 @@ with open('input', 'r') as input:
 
 
     print("Part 1: ", len(react(polymer)))  # 0:00:00.115975
-    print("Part 1-Time:", datetime.now() - start_time)
-
-    start_time = datetime.now()
     most_short = polymer
 
     for c in range(97, 123):
-        letter = chr(c)
-        letter_u = letter.upper()
-        most_short_candidate = react(polymer.replace(letter, '').replace(letter_u, ''))
+        most_short_candidate = react(polymer.replace(chr(c), '').replace(chr(c).upper(), ''))
         if len(most_short_candidate) < len(most_short):
             most_short = most_short_candidate
 
     print("Part 2: ", len(react(most_short)))
-    print("Part 2-Time", datetime.now() - start_time)
